b30f98c61ea28b8287516a1a8e9f3af7c37645e6
Meehyun Byeon <bmh2038@naver.com>
2023-07-17 21:43:31 +0900

    Section 4 - 스프링 빈과 의존관계(직접 스프링 빈 등록)

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -8,8 +8,17 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    // 생성자 injection => 권장
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    // setter injection(주입) => Alt+Ins => setter + Autowired
+    // 단점: 중간에 잘못 바꾸면 문제가 생김 => 누군가가 사용할 때 public으로 열려있어야 함
+//    private MemberService memberService;
+//    @Autowired
+//    public void setMemberService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
 }
